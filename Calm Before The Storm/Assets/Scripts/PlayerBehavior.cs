f5add800e682d5635eb8ad817b7cbacc49b8c7a6
JochenRobbrecht <jochen.robbrecht@student.howest.be>
2022-04-19 16:35:56 +0200

    Player/platrform colission

--- Calm Before The Storm/Assets/Scripts/PlayerBehavior.cs
+++ Calm Before The Storm/Assets/Scripts/PlayerBehavior.cs
@@ -5,13 +5,19 @@ using UnityEngine;
 public class PlayerBehavior : MonoBehaviour
 {
     private Rigidbody2D _rigidBody = null;
+    private Collider2D _collider = null;
     private const string _platformLayer = "Platform";
+    private const string _platformIgnore = "PlatformIgnore";
     [SerializeField]
-    private float _rayDistance = 0.1f;
+    private float _rayDistanceDown = 0.1f;
+    [SerializeField]
+    private float _rayDistanceUp = 1.0f;
+    private bool _isGrounded = false;
 
     void Start()
     {   
         _rigidBody = GetComponent<Rigidbody2D>();
+        _collider = GetComponent<Collider2D>();
     }
 
     void FixedUpdate()
@@ -26,16 +32,25 @@ public class PlayerBehavior : MonoBehaviour
         //if not negative y velocity return
         if (_rigidBody.velocity.y > 0.0f)
         {
-            _rigidBody.constraints = RigidbodyConstraints2D.None;
+            _isGrounded = false;
+            gameObject.layer = 6;
+
             return;
         }
 
-        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, -transform.up
-            , _rayDistance, LayerMask.GetMask(_platformLayer));
+        RaycastHit2D hitInfoDown = Physics2D.Raycast(transform.position, -transform.up
+            , _rayDistanceDown, LayerMask.GetMask(_platformLayer));
 
-        if(hitInfo.collider)
+        if(hitInfoDown.collider)
         {
-            _rigidBody.constraints = RigidbodyConstraints2D.FreezePositionY;
+            //if raycast up doesnt collide w platf 
+            _isGrounded = true;
+            gameObject.layer = LayerMask.GetMask("Default");
+        }
+        else
+        {
+            _isGrounded = false;
+            gameObject.layer = 6;
         }
     }
 }
