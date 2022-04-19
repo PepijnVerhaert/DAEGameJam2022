d5bd1fbdb6254f73d34c162386be49f98a8eedb9
Nasus <pepijn.verhaert@gmail.com>
2022-04-19 15:31:32 +0200

    BoatBehavior and scene

--- Calm Before The Storm/Assets/Scripts/BoatBehavior.cs
+++ Calm Before The Storm/Assets/Scripts/BoatBehavior.cs
@@ -2,8 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BoatLogic : MonoBehaviour
+public class BoatBehavior : MonoBehaviour
 {
+    
+
     // Start is called before the first frame update
     void Start()
     {
