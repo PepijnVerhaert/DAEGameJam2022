d03794ee13a572a7b3e3a7b2cfd38159c8d043c3
SariJanssen <61913528+dawndelirus@users.noreply.github.com>
2022-04-20 20:22:00 +0200

    fixed spawner to only spawn during storm

--- Calm Before The Storm/Assets/Scripts/AiSpawnerBehavior.cs
+++ Calm Before The Storm/Assets/Scripts/AiSpawnerBehavior.cs
@@ -45,6 +45,12 @@ public class AiSpawnerBehavior : MonoBehaviour
 
     private void Update()
     {
+        if (_isCalm)
+        {
+            _currentSpawnTime = 0f;
+            return;
+        }
+
         _currentSpawnTime += Time.deltaTime;
         if (_currentSpawnTime >= _spawnTime)
         {
