2d714c0d60ceebb729e64b532c7415b221a95e9f
Nasus <pepijn.verhaert@gmail.com>
2022-04-21 16:01:53 +0200

    seagull knockback

--- Calm Before The Storm/Assets/Scripts/SeagullBehavior.cs
+++ Calm Before The Storm/Assets/Scripts/SeagullBehavior.cs
@@ -7,7 +7,7 @@ public class SeagullBehavior : MonoBehaviour
     [SerializeField] private float _movementSpeed = 20f;
     [SerializeField] bool _isMovingLeft = false;
     [SerializeField] private float _stunDuration = 0.5f;
-    [SerializeField] private float _knockbackForce = 50f;
+    [SerializeField] private Vector2 _knockbackForce = new Vector2(500, 300);
     private int _moveDirection;
 
     private Collider2D[] _colliders = new Collider2D[4];
@@ -78,7 +78,7 @@ public class SeagullBehavior : MonoBehaviour
                 if (_colliders[i] == null)
                 {
                     collision.gameObject.GetComponent<PlayerBehavior>().Stun(_stunDuration);
-                    Vector2 knockback = new Vector2(_knockbackForce * _moveDirection, _knockbackForce);
+                    Vector2 knockback = new Vector2(_knockbackForce.x * _moveDirection, _knockbackForce.y);
                     collision.gameObject.GetComponent<Rigidbody2D>().AddForce(knockback);
                     _colliders[i] = collision;
                     return;
