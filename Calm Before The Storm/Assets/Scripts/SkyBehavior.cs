4595e4a99d2b699707e3cad1da4a71fffdb1420e
PepijnVerhaert <pepijn.verhaert@gmail.com>
2022-04-22 02:45:33 +0200

    lightning

--- Calm Before The Storm/Assets/Scripts/SkyBehavior.cs
+++ Calm Before The Storm/Assets/Scripts/SkyBehavior.cs
@@ -8,13 +8,25 @@ public class SkyBehavior : MonoBehaviour
     [SerializeField]
     private LoomingCloudBehavior _loomingCloud = null;
     [SerializeField]
+    private LoomingCloudBehavior[] _lightnings = null;
+    [SerializeField]
+    private Animator[] _lightningAnimators = null;
+    [SerializeField]
     private BackgroundBehavior _background = null;
+
+    [SerializeField]
+    private float _cycleOffsetMax = 1f;
 
     // Start is called before the first frame update
     void Start()
     {
         _cloudBehaviors = FindObjectsOfType<CloudBehavior>();
         //_loomingCloud = FindObjectOfType<LoomingCloudBehavior>();
+
+        for (int i = 0; i < _lightningAnimators.Length; i++)
+        {
+            if (_lightningAnimators[i]) _lightningAnimators[i].SetFloat("CycleOffset", Random.Range(0f, _cycleOffsetMax));
+        }
     }
 
     // Update is called once per frame
@@ -30,6 +42,15 @@ public class SkyBehavior : MonoBehaviour
             if(_cloudBehaviors[i]) _cloudBehaviors[i].ChangeWeather(isCalm);
         }
         if(_loomingCloud)_loomingCloud.ChangeWeather(isCalm);
+        for (int i = 0; i < _lightnings.Length; i++)
+        {
+            if (_lightnings[i]) _lightnings[i].ChangeWeather(isCalm);
+            if(_lightningAnimators[i]) _lightningAnimators[i].SetTrigger("Strike");
+        }
+
+        if (_lightnings[0])
+        {
+        }
         if(_background) _background.ChangeWeather(isCalm);
     }
 }
